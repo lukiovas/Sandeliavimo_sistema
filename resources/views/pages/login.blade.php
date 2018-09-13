cdcd27584d5d6083fbb04fea13bd2da83f039ea5
unknown <tuckuslukas@gmail.com>
2018-09-13 08:55:52 +0300

    'login_template'

---
+++ resources/views/pages/login.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.pageLayout')
+
+@section('content')
+    <div class="login-form">
+        <form action="" method="post">
+            <h2 class="text-center">Sign Up</h2>       
+            <div class="form-group">
+                <input type="text" name="username" class="form-control" placeholder="Username">
+            </div>
+            <div class="form-group">
+                <input type="password" name="password" class="form-control" placeholder="Password" required="required">
+            </div>
+            <div class="form-group">
+                <button type="submit" name="login" class="btn btn-primary btn-block">Log In</button>
+            </div>
+            <div class="clearfix">
+            </div>        
+        </form>
+    </div>
+@endsection       
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,6 +65,7 @@
         </style>
     </head>
     <body>
+        <h4>HI from Tomas</h4>
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
